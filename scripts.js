d409ce3a54d1624345368e0848715ec36a6a08bf
João Pedro Israel de Souza <juanpeterisrael@gmail.com>
2019-07-19 12:00:29 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const qualityList =  jQuery.makeArray($(".quality-list li"));
-const menuList = jQuery.makeArray($(".desktopNav li a, .mobileNav li a"));
-const windownHeight = $(window).height();
+const menuList = jQuery.makeArray($(".desktopNav li, .mobileNav li"));
+const windowHeight = $(window).height();
 
 $(document).ready(function() {
     //animate each element from the list
@@ -11,6 +11,36 @@ $(document).ready(function() {
         }, 1500);
         delay_time += 200;
     };
+    if ($(window).scrollTop() < windowHeight){
+        $(menuList).removeClass("active");
+        $(menuList[6]).addClass("active");
+        $(menuList[0]).addClass("active");
+    }
+    if ($(window).scrollTop() >= windowHeight -60){
+        $(menuList).removeClass("active");
+        $(menuList[7]).addClass("active");
+        $(menuList[1]).addClass("active");
+    }
+    if ($(window).scrollTop() >=  2*windowHeight -60){
+        $(menuList).removeClass("active");
+        $(menuList[8]).addClass("active");
+        $(menuList[2]).addClass("active");
+    }
+    if ($(window).scrollTop() >=  3*windowHeight -60){
+        $(menuList).removeClass("active");
+        $(menuList[9]).addClass("active");
+        $(menuList[3]).addClass("active");
+    }
+    if ($(window).scrollTop() >=  4*windowHeight -60){
+        $(menuList).removeClass("active");
+        $(menuList[10]).addClass("active");
+        $(menuList[4]).addClass("active");
+    }
+    if ($(window).scrollTop() >=  5*windowHeight -60){
+        $(menuList).removeClass("active");
+        $(menuList[11]).addClass("active");
+        $(menuList[5]).addClass("active");
+    }
     //animate svg when button is hovered
     // $('.white-btn').hover(function() {
     //     $('.page1 svg'){
@@ -22,29 +52,78 @@ $(document).ready(function() {
 });
 // Pretty cool function for adaptative window height scrollview
 $(window).scroll(function(){
-    if ($(window).scrollTop() <= windownHeight){
+    if ($(window).scrollTop() < windowHeight){
         $(menuList).removeClass("active");
-        $(menuList[0], menuList[6]).addClass("active");
-
+        $(menuList[6]).addClass("active");
+        $(menuList[0]).addClass("active");
     }
-    if ($(window).scrollTop() >= windownHeight -96){
+    if ($(window).scrollTop() >= windowHeight -60){
         $(menuList).removeClass("active");
-        $(menuList[1], menuList[7]).addClass("active");
+        $(menuList[7]).addClass("active");
+        $(menuList[1]).addClass("active");
     }
-    if ($(window).scrollTop() >=  2*windownHeight -96){
+    if ($(window).scrollTop() >=  2*windowHeight -60){
         $(menuList).removeClass("active");
-        $(menuList[2], menuList[8]).addClass("active");
+        $(menuList[8]).addClass("active");
+        $(menuList[2]).addClass("active");
     }
-    if ($(window).scrollTop() >=  3*windownHeight -96){
+    if ($(window).scrollTop() >=  3*windowHeight -60){
         $(menuList).removeClass("active");
-        $(menuList[3], menuList[9]).addClass("active");
+        $(menuList[9]).addClass("active");
+        $(menuList[3]).addClass("active");
     }
-    if ($(window).scrollTop() >=  4*windownHeight -96){
+    if ($(window).scrollTop() >=  4*windowHeight -60){
         $(menuList).removeClass("active");
-        $(menuList[4], menuList[10]).addClass("active");
+        $(menuList[10]).addClass("active");
+        $(menuList[4]).addClass("active");
     }
-    if ($(window).scrollTop() >=  5*windownHeight -96){
+    if ($(window).scrollTop() >=  5*windowHeight -60){
         $(menuList).removeClass("active");
-        $(menuList[5], menuList[11]).addClass("active");
+        $(menuList[11]).addClass("active");
+        $(menuList[5]).addClass("active");
     }
 });
+//Menu toggler
+$("#menu").click(function(){
+    if ($("#menu").attr("src") == "imgs/icons/menu.svg"){
+        $("#menu").attr("src","imgs/icons/cancel.svg");
+        $(".mobileNav").animate({
+            right: "0vw",
+        }, 500);
+        $(".overlay").css({
+            display: "block",
+        });
+    }
+    else {
+        $("#menu").attr("src","imgs/icons/menu.svg");
+        $(".mobileNav").animate({
+            right: "-60vw",
+        }, 500);
+        $(".overlay").css({
+            display: "none",
+        });
+    }
+});
+//add closing when clicking a link and when view goes to desktop
+$(".mobileNav a").click(function(){
+    $("#menu").attr("src","imgs/icons/menu.svg");
+    $(".mobileNav").animate({
+        right: "-60vw",
+    }, 500);
+    $(".overlay").css({
+        display: "none",
+    })
+})
+$(window).resize(function(){
+    if ($(window).width() >= 840) {
+        $(".overlay").css({
+            display: "none",
+        })
+        $("#menu").attr("src","imgs/icons/menu.svg");
+        $(".mobileNav").animate({
+            right: "-60vw",
+        }, 500);
+    }
+});
+
+//trigger a function every time the page is scrolled
